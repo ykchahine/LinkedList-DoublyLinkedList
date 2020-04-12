c6c3d15b184ebc813d53d8e2220f59f6afacd051
ykchahine <42751366+ykchahine@users.noreply.github.com>
2020-04-12 14:34:40 -0700

    Create dlist.c

---
+++ dlist.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "dlist.h"
+#include "utils.h"
+
+
+int main(int argc, const char* argv[]) {
+  
+  dlist_test();
+  return 0;
+}
